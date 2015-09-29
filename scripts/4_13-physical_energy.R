4cc1723288f8ef076fce399a13be11702ae38038
renatovargas <renovargas@gmail.com>
2015-09-29 13:31:17 -0600

    Added energy file

--- scripts/4_13-physical_energy.R
+++ scripts/4_13-physical_energy.R
@@ -4,7 +4,7 @@
 # SEEA AFF GT Github: https://github.com/renatovargas/seea-aff#
 
 # ------------------------------------------------------------------
-#        TABLE 4.13 PHYSICAL FLOW ACCOUNT FOR WATER
+#        TABLE 4.13 PHYSICAL FLOW ACCOUNT FOR ENERGY
 # ------------------------------------------------------------------
 
 # Comments start with #
@@ -37,96 +37,8 @@ rm(list=ls())
 currentdir <- getwd()
 
 
-# II. SUPPLY TABLE
-# ================
-
-# Connect to database
-drv <- dbDriver("PostgreSQL")
-con <- dbConnect(drv, dbname="naturacc_cuentas", 
-host="78.138.104.206", port="5432", user="naturacc_onil", 
-password="onilidb1234")
-
-# In order to avoid accented character encoding issues on Windows,
-sys <- Sys.info()["sysname"]
-if(sys["sysname"] == "Windows"){
-postgresqlpqExec(con, "SET client_encoding = 'windows-1252'");} 
-
-# Query database
-# We are interested in the output in tonnes for all 
-# agricultural products.
-
-# This query extracts the information that we need:
-sup <- dbGetQuery(con, "
-SELECT
-	scae.npg
-	,npg336.producto AS product
-	,scae.ntg
-	,ntg20.trans AS transaction
-	,scae.naeg
-	,naeg100.actividad AS industry
-	,CASE
-		WHEN 
-			scae.naeg BETWEEN 110 AND 750
-		AND
-			scae.ntg = 6001
-		THEN 	'1. Agricultural Industries'
-		WHEN
-			scae.naeg BETWEEN 810 AND 5910
-		AND
-			scae.ntg = 6001
-		THEN	'2. Manufacturing and other Industries'
-		WHEN
-			scae.naeg = 0
-		AND
-			scae.ntg = 6010
-		THEN 	'3. Imports'
-		WHEN    
-			scae.naeg=0 
-		AND
-			scae.ntg != 6010	
-		THEN	ntg20.trans
-	END AS seeaaff
-	,unidades.abrev
-	,scae.datofisico as ph_output
-FROM scae
-LEFT JOIN npg336
-  ON scae.npg = npg336.cod
-LEFT JOIN ntg20
-  ON scae.ntg = ntg20.cod
-LEFT JOIN naeg100
-  ON scae.naeg = naeg100.cod
-LEFT JOIN unidades
-  ON scae.dimfisico = unidades.id
-WHERE
-     scae.ann = 2010
-AND
-     scae.flujo = 1
-AND
-     (scae.ntg = 6001
-OR
-	scae.ntg = 6010)
-AND
-	scae.cuenta = 3
-ORDER BY
-     scae.ntg, seeaaff, scae.naeg, scae.npg;
-");
-
-dbDisconnect(con)
-dbUnloadDriver(drv)
-rm("con")
-rm("drv")
-
-# sup$product <- factor(sup$product, levels=unique(sup$product))
-# supplyTable <- as.table(xtabs(ph_output ~ product + seeaaff, data=sup))
-# supplyTable <- addmargins(supplyTable)
-# #colnames(supplyTable)[dim(table0401a)[2]] <- "Total Supply"
-# #rownames(supplyTable)[dim(table0401a)[1]] <- "Total"
-
-# Notes:
-
-
-# III. USE TABLE
-# ==============
+# II. ENERGY USE TABLE
+# ====================
 
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname="naturacc_cuentas", 
@@ -139,8 +51,8 @@ if(sys["sysname"] == "Windows"){
 postgresqlpqExec(con, "SET client_encoding = 'windows-1252'");} 
 
 # Query database
-# We are interested in the use in tonnes 2for all 
-# agricultural products.
+# We are interested in energy use for all 
+# agricultural industries mainly.
 
 # This query extracts the information that we need:
 use <- dbGetQuery(con, 
@@ -152,46 +64,64 @@ use <- dbGetQuery(con,
 	,scae.naeg
 	,naeg100.actividad AS industry
 	,CASE 
-		WHEN 
-			(scae.naeg BETWEEN 110 AND 520 OR
-			scae.naeg = 2040)
-		AND
-			scae.ntg = 6002
-		THEN 	'1. Agriculture'
+		WHEN
+			scae.naeg = 110
+		THEN 	'01. Coffee'
+		WHEN
+			scae.naeg = 120
+		THEN 	'02. Bananas'
+		WHEN
+			scae.naeg = 130
+		THEN 	'03. Cardamum'
+		WHEN
+			scae.naeg = 210
+		THEN 	'04. Cereals and other npc'
+		WHEN
+			scae.naeg = 220
+		THEN 	'05. Tubers, roots, vegetables, legumes, horticulture, and greenhouse crops'
+		WHEN
+			scae.naeg = 230
+		THEN 	'06. Fruits, nuts, teas, spices'
+		WHEN
+			scae.naeg = 240
+		THEN 	'07. Other crops (Food/non-food)'
+		WHEN
+			scae.naeg BETWEEN 310 AND 520
+		THEN 	'08. Livestock and livestock services (excl. vet.)'
 		WHEN
 			scae.naeg = 620
-		AND
-			scae.ntg = 6002
-		THEN	'2. Forestry'
+		THEN 	'09. Forestry'
 		WHEN
 			scae.naeg BETWEEN 710 AND 750
-		AND
-			scae.ntg = 6002
-		THEN	'3. Fisheries'
+		THEN 	'10. Fisheries and aquaculture'
 		WHEN
-			scae.naeg = 3810
-		AND
-			scae.ntg = 6002
-		THEN	'4. Water collection, treatment, and supply'
+			scae.naeg BETWEEN 810 AND 3620
+		THEN 	'11. Manufactures, mineral extraction, construction, and utilities (excl. electricity)'
 		WHEN
-			scae.naeg BETWEEN 810 AND 1020
-		AND
-			scae.ntg = 6002
-		THEN	'5. Mining'
+			scae.naeg BETWEEN 3810 AND 3910
+		THEN 	'11. Manufactures, mineral extraction, construction, and utilities (excl. electricity)'
 		WHEN
 			scae.naeg = 3710
-		AND
-			scae.ntg = 6002
-		THEN	'6. Electricity'
+		THEN 	'12. Electricity'
 		WHEN
-			(scae.naeg BETWEEN 1110 AND 3620 OR
-			scae.naeg BETWEEN 3910 AND 5910)
-		AND
-			scae.ntg = 6002
-		THEN	'7. Other industries'
-		WHEN
-			scae.naeg = 0
-		THEN 	ntg20.trans
+			scae.naeg BETWEEN 4010 AND 5910
+		THEN 	'13. Other industries'
+    WHEN
+      (scae.naeg = 0
+    AND
+      scae.ntg = 6150) 
+    THEN '14. Households'
+    WHEN
+      (scae.naeg = 0
+    AND
+      scae.ntg BETWEEN 6160 AND 6170) 
+    THEN '15. Other final consumption'
+    WHEN
+      (scae.naeg = 0
+    AND
+      scae.ntg BETWEEN 6040 AND 6050) 
+    THEN '16. Exports'
+    ELSE '17. Other adjustments'
     END as seeaaff
 	,unidades.abrev
 	,scae.datofisico as ph_output
@@ -209,12 +139,9 @@ WHERE
 AND
      scae.flujo = 2
 AND
-	scae.cuenta = 2
-AND
-    scae.npg = 470200
+	scae.cuenta = 4
 AND
      scae.ntg BETWEEN 6002 AND 6260
-
 ORDER BY
      scae.npg,seeaaff,scae.naeg;
      ")
@@ -236,10 +163,6 @@ useTable <- addmargins(useTable)
 # ============ 
 
 wb <- createWorkbook("SEEA AFF Guatemala")
-# ## Add a worksheet
-# addWorksheet(wb, "4.11 Water Abstraction")
-# ##write data to worksheet 1
-# writeData(wb, sheet = 1, supplyTable, rowNames = TRUE, startRow = 2)
 
 ## create and add a style to the column headers
 headerStyle <- createStyle(border="TopBottom", borderStyle ="medium", fontName="Arial Narrow", fontSize=10, textDecoration = "bold")
@@ -259,11 +182,11 @@ footerStyle <- createStyle(numFmt="COMMA", border="TopBottom", borderStyle ="med
 
 # Note that I changed worksheet 2 to 1 down here because there is no supply table
 
-addWorksheet(wb, "4.11 Water Abstraction")
+addWorksheet(wb, "4.13 Enegy Use")
 ##write data to worksheet 1
 writeData(wb, sheet = 1, useTable, rowNames = TRUE, startRow = 2)
 addStyle(wb, sheet = 1, headerStyle, rows = 2:2, cols = 1:(dim(useTable)[2]+1), gridExpand = TRUE)
-addStyle(wb, sheet = 1, bodyStyle, rows = 3:(3+dim(useTable)[1]), cols = 2:(dim(useTable)[1] + 1), gridExpand = TRUE)
+addStyle(wb, sheet = 1, bodyStyle, rows = 3:(3+dim(useTable)[1]), cols = 2:(dim(useTable)[2] + 1), gridExpand = TRUE)
 addStyle(wb, sheet = 1, rowNamesStyle, rows = 3:(dim(useTable)[1]+1), cols=1, gridExpand = TRUE)
 addStyle(wb, sheet = 1, footerStyle, rows = 1+(dim(useTable)[1] + 1), cols = 1:(dim(useTable)[2]+1), gridExpand = TRUE)
 setColWidths(wb, 1, cols=1:(dim(useTable)[2]+1), widths = "auto") 
@@ -271,12 +194,12 @@ setColWidths(wb, 1, cols=1:(dim(useTable)[2]+1), widths = "auto")
 
 
 ## set column width for row names column
-saveWorkbook(wb, "table0411.xlsx", overwrite = TRUE)
-openXL("table0411.xlsx")
+saveWorkbook(wb, "table0413.xlsx", overwrite = TRUE)
+openXL("table0413.xlsx")
 
 
 
 # Where is it?
-paste("Check out your file at: ",currentdir,"/table0411.xlsx.", " Enjoy!", sep = "")
+paste("Check out your file at: ",currentdir,"/table0413.xlsx.", " Enjoy!", sep = "")
 
 #rm(list=ls())
